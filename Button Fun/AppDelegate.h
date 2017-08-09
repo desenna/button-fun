f6b28db51b3ec4ea5d6f2ac0d82eefd085055519
Michael de Senna <desenna@gmail.com>
2017-08-08 20:44:31 -0400

    First commit

---
+++ Button Fun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Button Fun
+//
+//  Created by Michael on 4/6/16.
+//  Copyright © 2016 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Button Fun/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Button Fun
+//
+//  Created by Michael on 4/6/16.
+//  Copyright © 2016 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UICollectionView *cv;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *box;
+
+@end
+
